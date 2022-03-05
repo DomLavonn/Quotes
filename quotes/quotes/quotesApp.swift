4dd22f3f19ba67321d596ff0ad7bcaa9dc03f740
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-03-05 18:33:44 -0500

    Add directory

---
+++ quotes/quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by Domanik Johnson on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
